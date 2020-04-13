c128693bd06ccff72c347074d33b88754933c132
Ekaterina Klueva <catherine.klueva@gmail.com>
2020-04-13 14:05:39 +0300

    feat: add subtask3

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,43 @@
 package subtask4
 
+import java.lang.StringBuilder
+
 class Pangram {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        val vowels = setOf('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y')
+        var tempString = inputString.replace("""[^a-zA-Z]""".toRegex(), "").toLowerCase()
+        tempString = tempString.replace("""(.)(?=.*\1)""".toRegex(), "")
+        val words = inputString.split(" ")
+        val numbersAndWords = ArrayList<Pair<Int, String>>()
+        if (tempString.length == 26) {
+            for (word in words) {
+                var numberOfVowels = 0
+                val st = StringBuilder(word)
+                for (index in st.indices) {
+                    if (st[index] in vowels) {
+                        numberOfVowels++
+                        st[index] = st[index].toUpperCase()
+                    }
+                }
+                numbersAndWords.add(numberOfVowels to st.toString())
+            }
+        } else {
+            for (word in words) {
+                var numberOfVowels = 0
+                val numberOfNonLetters = word.replace("""[a-zA-Z]""".toRegex(), "").length
+                val st = StringBuilder(word)
+                for (index in st.indices) {
+                    if (st[index] in vowels) {
+                        numberOfVowels++
+                    } else {
+                        st[index] = st[index].toUpperCase()
+                    }
+                }
+                numbersAndWords.add(st.length - numberOfVowels - numberOfNonLetters to st.toString())
+            }
+        }
+        numbersAndWords.sortBy { it.first }
+        return numbersAndWords.joinToString(" ") { it -> "${it.first}${it.second}"}
     }
 }
